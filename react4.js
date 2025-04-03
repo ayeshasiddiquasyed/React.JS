dc00055469e40d5990926a472c01a9c0f343f4d5
Syed.Ayesha Siddiqua <syedayeshasiddiqua57@gmail.com>
2025-04-03 22:04:27 +0530

    Add files via upload

---
+++ react4.js
@@ -0,0 +1,10 @@
+"use client";
+import React,{Component} from "react";
+class App extends Component{
+  render(){
+    return (
+      <div>this example contains JSX</div>
+    )
+  }
+}
+export default App;
