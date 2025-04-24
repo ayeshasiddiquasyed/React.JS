c718d42e1fc2d9ca3f235b5cf057c35343cfa3f8
Syed.Ayesha Siddiqua <syedayeshasiddiqua57@gmail.com>
2025-04-24 20:30:04 +0530

    Add files via upload

---
+++ react27.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+class App extends React.Component{
+render(){
+  return(
+  <>
+    <h2>Hello World</h2>
+    <p>welcome</p>
+  </>
+);
+}
+}
+export default App;
