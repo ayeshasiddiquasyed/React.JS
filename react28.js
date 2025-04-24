339dee840aa269434fb6203b8e2110218a2c7ad1
Syed.Ayesha Siddiqua <syedayeshasiddiqua57@gmail.com>
2025-04-24 20:30:26 +0530

    Add files via upload

---
+++ react28.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+class App extends React.Component{
+render(){
+  return(
+  <React.Fragment>
+    <h2>Hello World</h2>
+    <p>welcome</p>
+  </React.Fragment>
+);
+}
+}
+export default App;
